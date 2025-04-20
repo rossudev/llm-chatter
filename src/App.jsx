9915d71803f755e87432801ee3d3e652dc58429d
Anonymous <inquiries@mammothpublishing.org>
2025-04-19 23:34:53 -0400

    v0.1.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
 import Chatter from './Chatter.jsx';
-import Shared from './Shared.jsx';
+
+const Shared = lazy(() => import('./Shared.jsx'))
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Chatter />} />
-        <Route path="/shared/:userName/:uniqueId" element={<Shared />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Chatter />} />
+          <Route path="/shared/:userName/:uniqueId" element={<Shared />} />
+          <Route path="*" element={<Chatter />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
